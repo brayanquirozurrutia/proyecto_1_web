26904ef4d4f031e34d8bef6f207961b007800f11
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-03-11 16:17:53 -0300

    :construction: Se conecta a endpoint para inciar sesión

--- src/components/login/Loginform.tsx
+++ src/components/login/Loginform.tsx
@@ -1,16 +1,52 @@
+import React, { useState } from "react";
+
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/neighborhood/v1/users/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error response:", errorData);
+        throw new Error("Authentication failed");
+      }
+      const data = await response.json();
+      console.log("Authentication success:", data);
+      localStorage.setItem("token", data.access);
+    } catch (error) {
+      console.error("Authentication error:", error);
+      if (error instanceof Response) {
+        const data = await error.json();
+        console.error("Error response:", data);
+      }
+    }
+  };
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="mb-4 flex-col">
           <div className="py-1">
-            <label htmlFor="" className="text-slate-700">
+            <label htmlFor="email" className="text-slate-700">
               Email
             </label>
           </div>
           <div>
             <input
               type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="border border-gray-400 rounded py-1 px-2 w-full focus:outline-none focus:ring focus:border-blue-500 focus:shadow-2xl"
               placeholder="m@example.com"
             />
@@ -18,24 +54,32 @@ function LoginForm() {
         </div>
         <div className="mb-4 flex-col">
           <div className="py-1">
-            <label htmlFor="" className="text-slate-700">
+            <label htmlFor="password" className="text-slate-700">
               Password
             </label>
           </div>
           <div>
             <input
               type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="border border-gray-400 rounded py-1 px-2 w-full focus:outline-none focus:ring focus:border-blue-500 focus:shadow-2xl"
             />
           </div>
         </div>
         <div className="mb-4">
-          <button className="bg-gray-950 text-white rounded w-full py-1 hover:bg-gray-800 hover:shadow-2xl">
+          <button
+            type="submit"
+            className="bg-gray-950 text-white rounded w-full py-1 hover:bg-gray-800 hover:shadow-2xl"
+          >
             Sign In
           </button>
         </div>
         <div className="mb-4 text-center">
-          <a href="" className="underline underline-offset-2">Forgot Password?</a>
+          <a href="" className="underline underline-offset-2">
+            Forgot Password?
+          </a>
         </div>
         <div className="mb-4 font-bold hover:underline hover:underline-offset-2">
           <a href="">Sign up for an account?</a>
@@ -44,6 +88,5 @@ function LoginForm() {
     </div>
   );
 }
-
 
 export default LoginForm;
