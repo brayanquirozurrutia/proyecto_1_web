7e205e6a090615ce902ce6012ead9355f592e042
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-03-11 11:59:10 -0300

    :construction: FEAT: Diseño página principal 3

--- src/components/NeighborhoodLogo.tsx
+++ src/components/NeighborhoodLogo.tsx
@@ -1,4 +1,4 @@
-
+import '../styles/neighborhoodlogo.css';
 
 interface Props {
   widthClass?: string;
@@ -7,16 +7,16 @@ interface Props {
 function NeighborhoodLogo({ widthClass = "w-full" }: Props) {
   return (
     <div
-      className="flex justify-center items-center h-full" // Agrega clases flex y centrado vertical
-      style={{ height: "100%" }} // Asegúrate de que el div tenga una altura definida
+      className="flex justify-center items-center h-full"
+      style={{ height: "100%" }}
     >
       <picture>
         <source srcSet="src/assets/logo.webp" type="image/webp" />
-        <source srcSet="src/assets/logo.png" type="image/png" />
+        <source srcSet="src/assets/logo.png" type="image/png" /> 
         <img
           src="src/assets/logo.png"
           alt="logo"
-          className={`${widthClass} h-5/6`}
+          className={`${widthClass} h-5/6 pulse`}
         />
       </picture>
     </div>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,19 +1,24 @@
-
 import LoginText from "./LoginText";
 import LoginForm from "./Loginform";
 import NeighborhoodLogo from "../NeighborhoodLogo";
+import FooterText from "../FooterText";
 
 function Login() {
   return (
-    <div className="w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12 bg-slate-200 shadow-2xl rounded-2xl mx-auto p-2 my-4">
-      <div className="flex justify-center">
-        <div className="w-full sm:w-10/12 md:w-9/12 xl:w-7/12 px-10 py-10">
-          <LoginText />
-          <LoginForm />
+    <div >
+      <div className="w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12 shadow-2xl rounded-2xl mx-auto p-2 my-4 bg-gradient-to-b from-slate-300 via-slate-200 to-slate-300">
+        <div className="flex justify-center">
+          <div className="w-full sm:w-10/12 md:w-9/12 xl:w-7/12 px-10 py-10">
+            <LoginText />
+            <LoginForm />
+          </div>
+          <div className="w-full hidden lg:block lg:w-1/2">
+            <NeighborhoodLogo />
+          </div>
         </div>
-        <div className="w-full hidden lg:block lg:w-1/2">
-        <NeighborhoodLogo/>
-        </div>
+      </div>
+      <div className="sm:pb-4 md:pb-4 lg:pb-3 xl:pb-2 2xl:pb-1">
+        <FooterText />
       </div>
     </div>
   );
